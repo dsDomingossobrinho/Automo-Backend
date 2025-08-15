22be0797eb54925d109326028790a2c9dac3a2b9
dsDomingossobrinho <88497747+DomingosSobrinhoDS@users.noreply.github.com>
2025-08-15 22:56:50 +0100

    initial config

---
+++ src/main/java/com/automo/AutomoApplication.java
@@ -0,0 +1,13 @@
+package com.automo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomoApplication.class, args);
+	}
+
+}
